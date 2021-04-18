a3899d7ca19176a1329754964b97b9fef904f7ad
Artemiy Nastachenko <artemiynastachenko@gmail.com>
2021-04-18 22:47:54 +0300

    init: HW2 solution

---
+++ task2/nonRepeatingSymbols/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace nonRepeatingSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Your string is empty or null");
+            }
+
+            else
+            {
+                Console.WriteLine(symbolsCounter(args[0].ToLower()));
+            }
+
+            Console.ReadKey();
+        }
+
+        public static int SymbolsCounter(string myString)
+        {
+            int counter = 1;
+            int resultCounter = 1;
+            int cuttedLength = myString.Length - 1;
+            for (int i = 0; i < cuttedLength; i++)
+            {
+                if (myString[i] != myString[i + 1])
+                {
+                    counter++;
+                }          
+                
+                else
+                {
+                    if (counter > resultCounter)
+                    {
+                        resultCounter = counter;
+                    }
+
+                    counter = 1;
+                }
+            }
+
+            return Math.Max(resultCounter, counter);
+        }
+    }
+}
